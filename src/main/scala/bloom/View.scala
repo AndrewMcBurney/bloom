a864a5ad4b8a5a9a27645dd0b16cd76e234525df
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-04-29 12:34:02 -0400

    Updated controller addCategory method.

--- src/main/scala/bloom/View.scala
+++ src/main/scala/bloom/View.scala
@@ -12,8 +12,11 @@ package bloom
 import bloom.model.Model
 
 import org.scalajs.dom
+import org.scalajs.dom.html
 import org.scalajs.jquery.jQuery
 import scala.scalajs.js.JSApp
+import org.scalajs.dom.ext.KeyCode
+import org.scalajs.dom.KeyboardEvent
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 object Main extends JSApp {
@@ -22,22 +25,34 @@ object Main extends JSApp {
 
   // Add Category with name
   @JSExportTopLevel("addCategory")
-  def addCategory(): Unit = {
-    Model.addCategory("category_name")
-    jQuery(() => updateTodoList())
+  def addCategory(in: html.Input): Unit = {
+    in.onkeydown = { (e: KeyboardEvent) =>
+      // Add a category if the keyCode is Enter
+      if (e.keyCode == KeyCode.Enter) {
+        // Add Category in model
+        Model.addCategory(in.value)
+
+        // Redraw todoList
+        jQuery(() => updateTodoList())
+      }
+    }
   }
 
   // Adds Todo for a given category
   @JSExportTopLevel("addTodo")
-  def addTodo(): Unit = {
-    Model.getCategoryByID(0).addTodo("one")
-    jQuery(() => updateTodoList())
+  def addTodo(in: html.Input): Unit = {
+    in.onkeydown = { (e: KeyboardEvent) =>
+      if (e.keyCode == KeyCode.Enter) {
+        Model.getCategoryByID(0).addTodo("one")
+        jQuery(() => updateTodoList())
+      }
+    }
   }
 
   /** View Methods */
 
   // Updates view todoList with data from chrome storage
-  def updateTodoList(): Unit = jQuery("#todos").html(Model.toHTML)
+  def updateTodoList(): Unit = jQuery("#todo-list").html(Model.toHTML)
 
   // Initial UI setup
   def setUpUI(): Unit = {
